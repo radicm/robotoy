970514104ea83e7d110d1c03ae09e57185274589
RadicM <milosrt2011@gmail.com>
2019-07-08 11:16:46 +0200

    Added robot logic

---
+++ lib/robotoy/robot.rb
@@ -0,0 +1,96 @@
+module Robotoy
+  class Robot
+
+    X_LENGTH = 5
+    Y_LENGTH = 5
+
+    NORTH = "NORTH".freeze
+    SOUTH = "SOUTH".freeze
+    EAST = "EAST".freeze
+    WEST = "WEST".freeze
+
+    LEFT = "LEFT".freeze
+    RIGHT = "RIGHT".freeze
+
+    MOVE_STEP = 1
+
+    # @param [Integer] x_length
+    # @param [Integer] y_length
+    def initialize(x_length = X_LENGTH, y_length = Y_LENGTH)
+      raise "Table length can't be smaller that 1" if x_length < 1 || y_length < 1
+
+      @x_length = x_length
+      @y_length = y_length
+      @x = 0
+      @y = 0
+
+      north = Direction.new(WEST, EAST, NORTH)
+      east = Direction.new(NORTH, SOUTH, EAST)
+      south = Direction.new(EAST, WEST, SOUTH)
+      west = Direction.new(SOUTH, NORTH, WEST)
+
+      north.left = west; north.right = east
+      east.left = north; east.right = south
+      south.left = east; south.right = west
+      west.left = south; west.right = north
+
+      @directions = [north,east,south,west]
+
+      @direction = nil
+    end
+
+    def move
+      case @direction.current
+      when NORTH
+        r = @x + MOVE_STEP
+        @x = r if r < @x_length
+      when EAST
+        r = @y + MOVE_STEP
+        @y = r if r < @y_length
+      when SOUTH
+        r = @x - MOVE_STEP
+        @x = r unless r.negative?
+      when WEST
+        r = @y - MOVE_STEP
+        @y = r unless r.negative?
+      else
+        raise "Unsupported direction!"
+      end
+    end
+
+    # @param [Integer] x
+    # @param [Integer] y
+    # @param [String] direction
+    def place(x, y, direction)
+      raise "Invalid coordinates!" if x.negative? || y.negative?
+
+      @x = x
+      @y = y
+      @direction = @directions.select { |n| n.current == direction }.first
+
+      raise "Invalid direction!" if @direction.nil?
+    end
+
+    # @return [String]
+    def report
+      puts "Output: #{@x},#{@y},#{@direction.current}"
+    end
+
+    # @param [String] direction
+    def rotate(direction)
+      @direction = case direction
+                   when LEFT
+                     @direction.left
+                   when RIGHT
+                     @direction.right
+                   else
+                     raise "Invalid direction!"
+                   end
+      true
+    end
+
+    private
+
+    class Direction < Struct.new(:left, :right, :current); end
+  end
+end
---
+++ spec/robotoy/robot_spec.rb
@@ -0,0 +1,79 @@
+require "spec_helper"
+
+describe Robotoy::Robot do
+
+  subject { described_class.new }
+
+  let(:x) { 1 }
+  let(:y) { 1 }
+  let(:direction) { Robotoy::Robot::NORTH }
+
+  describe "place" do
+    context "place with invalid coordinate" do
+      it "raises runtime error" do
+        expect { subject.place(1, -1, direction) }.to raise_error(RuntimeError, "Invalid coordinates!")
+      end
+
+      it "raises runtime error" do
+        expect { subject.place(-1, 1, direction) }.to raise_error(RuntimeError, "Invalid coordinates!")
+      end
+    end
+
+    context "place with invalid direction" do
+      it "raises runtime error" do
+        expect { subject.place(1, 1, "xyz") }.to raise_error(RuntimeError, "Invalid direction!")
+      end
+    end
+  end
+
+  describe "rotate" do
+    before do
+      subject.place(x, y, direction)
+    end
+
+    it "rotates to right" do
+      subject.rotate(Robotoy::Robot::RIGHT)
+      expect { subject.report }.to output("Output: #{x},#{y},#{Robotoy::Robot::EAST}\n").to_stdout
+    end
+
+    it "rotates to left" do
+      subject.rotate(Robotoy::Robot::LEFT)
+      expect { subject.report }.to output("Output: #{x},#{y},#{Robotoy::Robot::WEST}\n").to_stdout
+    end
+
+    it "rotates to left" do
+      expect { subject.rotate("xyz") }.to raise_error(RuntimeError, "Invalid direction!")
+    end
+  end
+
+  describe "move" do
+    it do
+      subject.place(0, 0, Robotoy::Robot::NORTH)
+      subject.move
+      expect { subject.report }.to output("Output: 1,0,NORTH\n").to_stdout
+    end
+
+    it do
+      subject.place(0, 0, Robotoy::Robot::NORTH)
+      subject.rotate(Robotoy::Robot::LEFT)
+      subject.move
+      expect { subject.report }.to output("Output: 0,0,WEST\n").to_stdout
+    end
+
+    it do
+      subject.place(2, 1, Robotoy::Robot::EAST)
+      subject.move
+      subject.move
+      subject.rotate(Robotoy::Robot::LEFT)
+      subject.move
+      expect { subject.report }.to output("Output: 3,3,NORTH\n").to_stdout
+    end
+  end
+
+  describe "report" do
+    it "prints current position and direction" do
+      subject.place(x, y, direction)
+      expect { subject.report }.to output("Output: #{x},#{y},#{direction}\n").to_stdout
+    end
+  end
+end
