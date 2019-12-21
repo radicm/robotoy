7892fc15959e0996116f89efd15675bee2cffdec
radicm <milos.radic@consulteer.com>
2019-12-21 15:05:36 +0100

    updated loading path

--- bin/start.rb
+++ bin/start.rb
@@ -1,12 +1,9 @@
 #!/usr/bin/env ruby
 
-# TODO load path should be handled in a better way with ruby -Idirectory or -Cdirectory
-# TODO but I didn't had enough time to make it work
-
-dir = File.expand_path(File.dirname(__FILE__))
-dir.gsub!('bin', 'lib')
+dir = File.expand_path("../lib", __FILE__)
+dir = dir.gsub('/bin/lib', '/lib')
 $LOAD_PATH.unshift(dir) unless $LOAD_PATH.include?(dir)
 
-require '../lib/robotoy'
+require 'robotoy'
 
 Robotoy.start_game
